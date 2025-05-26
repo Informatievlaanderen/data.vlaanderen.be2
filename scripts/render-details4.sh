d9c5dfd3eb2623038172160864b8c8bc66f888c7
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2025-05-26 14:53:59 +0200

    New run

--- scripts/render-details4.sh
+++ scripts/render-details4.sh
@@ -387,9 +387,9 @@ validate_jsonld() {
         MERGEDFILE=${JSONI}
     fi
 
-    mkdir -p ${RLINE}
+    mkdir -p ${TLINE}
 
-    REPORTFILE=${RLINE}/jsonld-validation.report.md
+    REPORTFILE=${TLINE}/jsonld-validation.report.md
     echo "${REPORTLINEPREFIX}oslo-jsonld-validator for language ${LANGUAGE}${REPORTLINENEWLINE}" &>>${REPORTFILE}
     echo "${REPORTLINEPREFIX}-------------------------------------${REPORTLINENEWLINE}" &>>${REPORTFILE}
     oslo-jsonld-validator --input ${MERGEDFILE} \
@@ -397,6 +397,7 @@ validate_jsonld() {
         >>${REPORTFILE} 2>&1
 
     cat ${REPORTFILE}
+    echo ${TLINE}/jsonld-validation.report.md
 }
 
 render_translationfiles() {
