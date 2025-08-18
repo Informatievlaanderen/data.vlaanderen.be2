824a10fb662f2c66e6cededf455e108b704ba272
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2025-08-18 14:54:02 +0200

    Allow a full run to see fully constructed urlref in action

--- scripts/checkoutRepositories.sh
+++ scripts/checkoutRepositories.sh
@@ -132,9 +132,6 @@ construct_urlref_if_missing() {
                 if [[ "$pub_state" =~ StandaardStatus/([^/]+)$ ]]; then
                     pub_state="${BASH_REMATCH[1],,}"
                 fi
-
-                echo "${pub_date} ${pub_state}"
-
                 
                 echo "Retrieved from metadata: pub_date=$pub_date, pub_state=$pub_state, type=$type"
                 
