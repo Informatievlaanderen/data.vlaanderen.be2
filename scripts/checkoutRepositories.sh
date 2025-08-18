44769e5d3a52f137e250f6f914c01683a556b6ee
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2025-08-18 14:43:35 +0200

    Run

--- scripts/checkoutRepositories.sh
+++ scripts/checkoutRepositories.sh
@@ -116,11 +116,35 @@ construct_urlref_if_missing() {
             fi
             
             if [[ -f "$temp_metadata" ]]; then
+
+#             [
+#  {
+#    "name": "leermiddelen-ap",
+#    "type": "ap",
+#    "eap": "Leermiddelen.eap",
+#    "diagram": "OSLO-Leermiddelen",
+#    "template": "leermiddelen-ap.j2",
+#    "title": "Applicatieprofiel Leermiddelen",
+#    "publication-state": "https://data.vlaanderen.be/id/concept/StandaardStatus/KandidaatStandaard",
+#    "publication-date": "2025-08-01",
+#    "license": "https://data.vlaanderen.be/id/licentie/modellicentie-gratis-hergebruik/v1.0",
+#    "contributors-file": "stakeholders.csv",
+#    "contributors-column": "Rol",
+#    "site": "site-skeleton/leermiddelen-ap",
+#    "feedbackurl": "https://github.com/Informatievlaanderen/OSLOthema-leermiddelen/issues",
+#    "standaardregisterurl": "https://data.vlaanderen.be/standaarden/applicatieprofiel-leermiddelen"
+#  }
+# ]
                 # Extract metadata from the thema repository - INCLUDING TYPE
                 cat "$temp_metadata"
-                local pub_date=$(jq -r ".[] | select(.name == \"$name\") | .publication_date // empty" "$temp_metadata")
-                local pub_state=$(jq -r ".[] | select(.name == \"$name\") | .publication_state // empty" "$temp_metadata")
-                local type=$(jq -r ".[] | select(.name == \"$name\") | .type // empty" "$temp_metadata")
+                local meta_obj=$(jq -c ".[] | select(.name == \"$name\")" "$temp_metadata")
+                if [[ -z "$meta_obj" ]]; then
+                    echo "Warning: No metadata found for name $name in $temp_metadata"
+                    return
+                fi
+                local pub_date=$(echo "$meta_obj" | jq -r '.publication_date // empty')
+                local pub_state=$(echo "$meta_obj" | jq -r '.publication_state // empty')
+                local type=$(echo "$meta_obj" | jq -r '.type // empty')
 
                 
                 echo "Retrieved from metadata: pub_date=$pub_date, pub_state=$pub_state, type=$type"
