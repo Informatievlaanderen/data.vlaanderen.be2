958befe47488476968c020467f6fe0eaede95f89
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2025-08-25 13:05:30 +0200

    Run metadata

--- scripts/extract-what-4.sh
+++ scripts/extract-what-4.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source "$(dirname "$0")/generator-parameters.sh"
 # for debugging purposes
 #set -x
 
@@ -19,6 +18,30 @@ execution_strickness() {
         exit -1
     fi
 }
+
+generator_parameters() {
+
+    local GENERATOR=$1
+    local JSONI=$2
+
+    #
+    # The toolchain can add specific parameters for the SHACL generation tool
+    # Priority rules are as follows:
+    #   1. publication point specific
+    #   2. generic configuration
+    #   3. otherwise empty string
+    #
+    COMMAND=$(echo '.'${GENERATOR}'.parameters')
+    PARAMETERS=$(jq -r ${COMMAND} ${JSONI})
+    if [ "${PARAMETERS}" == "null" ]; then
+        PARAMETERS=$(jq -r ${COMMAND} ${CONFIGDIR}/config.json)
+    fi
+    if [ "${PARAMETERS}" == "null" ] || [ -z "${PARAMETERS}" ]; then
+        PARAMETERS=""
+    fi
+}
+
+
 
 #############################################################################################
 # extraction command functions
