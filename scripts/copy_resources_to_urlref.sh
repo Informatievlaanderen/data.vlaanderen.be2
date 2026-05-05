0fd1116a405af9c451ecdbf3d1bcc1210ac1c16a
OSLO-support <OSLO-support@vlaanderen.be>
2026-05-05 09:59:38 +0200

    Run persoon-im

--- scripts/copy_resources_to_urlref.sh
+++ scripts/copy_resources_to_urlref.sh
@@ -119,6 +119,7 @@ fetch_external_jsonld() {
     }
     
     normalized_url=$(normalize_namespace_url "$source_url")
+    echo "Trying to fetch ontology: $normalized_url"
     
     if [ -z "$normalized_url" ]; then
         return 1
@@ -141,7 +142,7 @@ fetch_external_jsonld() {
     if node "$SCRIPTDIR/fetch_external_rdf.js" "$normalized_url" "$target_file"; then
         return 0
     fi
-
+    
     echo "Failed to fetch external source: $normalized_url"
     mkdir -p "$target_dir"
     touch "$failed_cache"
