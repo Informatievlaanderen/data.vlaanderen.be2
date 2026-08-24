e62533066bdd52b884190efca97b968692d37cd6
OSLO-support <OSLO-support@vlaanderen.be>
2026-08-24 23:27:59 +0200

    Rerun

--- scripts/render-details4.sh
+++ scripts/render-details4.sh
@@ -1170,7 +1170,7 @@ render_swagger() {
         --description "This is a inspirational OpenAPI Swagger publication" \
         --contextURL ${CONTEXT_URL} \
         --baseURL ${HOSTNAME} \
-        --primaryLanguage ${PRIMELANGUAGE} \
+        # --primaryLanguage ${PRIMELANGUAGE} \
         ${PARAMETERS} \
         &>>${REPORTFILE}
         
